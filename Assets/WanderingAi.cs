585a5d3e1c6c345c2ef7488b2f6591f508ce34b2
qhc99 <onlinecqh99@gmail.com>
2024-10-25 20:00:06 -0400

    init

---
+++ Assets/WanderingAi.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAi : MonoBehaviour
+{
+    public float speed = 3.0f;
+    public float obstacleRange = 5.0f;
+    private bool isAlive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAlive)
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            Ray ray = new Ray(transform.position, transform.forward);
+            if (Physics.SphereCast(ray, 0.75f, out RaycastHit hit))
+            {
+                if (hit.distance < obstacleRange)
+                {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+    }
+
+    public void SetAlive(bool alive)
+    {
+        isAlive = alive;
+    }
+}
